ca2da838eb457dbac92b793fa9c1d1fb25ad6c14
mainaanthony <mainaanthonymwai@gmail.com>
2023-06-30 11:27:42 +0300

    week 9 assessment

--- index.js
+++ index.js
@@ -1,206 +1,141 @@
-// const balance = document.querySelector("#balance");
-// const inc_amt = document.querySelector("#inc-amt");
-// const exp_amt = document.querySelector("#exp-amt");
-// const trans = document.querySelector("#trans");
-// const form = document.querySelector("#form");
-// const description = document.querySelector("#desc");
-// const amount = document.querySelector("#amount")
+
+  
+
+  ///week 9 test assessment
+
+  
+  //question one
+function setCallBackInvokeAfter2seconds(callback){
+
+setTimeout(callback, 2000)
+
+}
+
+function callbackToBeCalled(){
+   console.log("Call this")
+
+}
+
+setCallBackInvokeAfter2seconds(callbackToBeCalled)
+
+//question two
 
 
-// const listings = document.querySelector('.trans')
+  class Employee {
+    constructor(name , salary) {
+      this.name = name;
+      this.salary = salary;
+     }
+  
 
 
-// const list = (transaction)=>{
+     calculateSalary(){
+           
+      let annualSalary = (this.salary  * 12)
+
+     console.log(` ${this.name} is to be paid ${annualSalary}`)
+
+     }
+    
+  }
+  
+  // Creating an instance of the Employee 
+  const myEmployee = new Employee("Anthony Maina", 1000);
+  
+  calculate_sum_of_numbers(salary)
 
 
-// const sign = transaction.amount < 0 ? "-" : "+"    
-// const item = document.createElement('li')
-// item.classList.add(transaction.amount < 0 ?  "exp" : "inc")
-// item.innerHTML =  `${transaction.description} <span> ${sign} ${Math.abs(transaction.amount)} </span>
-//  <button class="btn-del" onclick="removeTrans(${transaction.id})"> del</button>`
- 
- 
-//  trans.appendChild(item)
-//  console.log(transaction)
-// }
+  class Manager extends Employee{
+     constructor(name, salary, department){
+      super(name, salary)
+      this.department = department
 
+     }
 
-// const localStorageTrans = JSON.parse(localStorage.getItem("trans"));
-// let transactions = localStorage.getItem("trans") !== null ? localStorageTrans : [];
+     calculateAnnualSalary(bonus){
+       const originalSalary = super.calculateSalary()
 
+       managerAnnualSalary = (bonus * originalSalary) + originalSalary
+       
+    return managerAnnualSalary
 
-
-
-// function addTransaction(e){
-//   e.preventDefault();
-//   if(description.value.trim() == "" || amount.value.trim() == ""){
-//     alert("Please enter a description and amount")
-
-//   }else{
-//     const transaction = {
-//         id : uniqueId(),
-//         description: description.value,
-//         amount: +amount.value
-//     };
-//     transactions.unshift(transaction)
-//     list (transaction)
-//     description.value = "";
-//     amount.value = "";
-//     updateLocalStorage()
+     }
     
 
-//   }
-// }
 
-
-// function removeTrans(id) {
-//   if (confirm("Are you sure you want to delete this Transaction?")) {
-//     transactions = transactions.filter((transaction) => transaction.id != id);
-//     config();
-//     updateLocalStorage();
-//   } else {
-//     return;
-//   }
-// }
-
-// form.addEventListener("submit" , addTransaction)
-
-
-
-// function uniqueId(){
-//     return Math.floor(Math.random()  * 100000)
-
-// }
-
-
-
-
-
-// function updateLocalStorage() {
-//   localStorage.setItem("trans", JSON.stringify(transactions));
-// }
-
-
-// function config() {
-//   trans.innerHTML = "";
-//   transactions.forEach(list);
- 
-// }
-
-
-
-class BankAccount {
-  constructor(accountNumber, accountHolder, balance) {
-    this.accountNumber = accountNumber;
-    this.accountHolder = accountHolder;
-    this.balance = balance;
   }
 
-  deposit(amount) {
-    this.balance += amount;
-    console.log(`Deposited ${amount} into account ${this.accountNumber}. New balance: ${this.balance}`);
+  //creating instances of the managers
+
+const manager1 = new Manager("Carlos Gambino", 5000, "Quality Assurance")
+const annualSalary1 = manager1.calculateAnnualSalary(0.2)
+
+
+console.log(`${manager1.name} in the ${manager1.department} is paid an Annual salary of ${annualSalary1}`)
+
+const manager2 = new Manager("Macy price", 5000, "Human Resource")
+const annualSalary2 = manager2.calculateAnnualSalary(0.2)
+
+console.log(`${manager2.name} in the ${manager2.department} is paid an Annual salary of ${annualSalary2}`)
+
+
+  //question three
+
+function year_range_to_calculate_from(start_year, end_year){
+
+  let year_range = []
+
+  for (let i = start_year; i<= end_year; i++){
+
+        year_range.push(i)
   }
 
-  withdraw(amount) {
-    if (this.balance >= amount) {
-      this.balance -= amount;
-      console.log(`Withdrawn ${amount} from account ${this.accountNumber}. New balance: ${this.balance}`);
-    } else {
-      console.log(`Insufficient funds in account ${this.accountNumber}. Current balance: ${this.balance}`);
+  let the_new_array = []
+
+  year_range.forEach(
+    function(year){
+    if(look_for_Leap_year(year)){
+
+      the_new_array.push(year)
     }
-  }
-
-  getAccountNo() {
-    console.log(`Account Information:
-      Account Number: ${this.accountNumber}
-      Account Holder: ${this.accountHolder}
-      Balance: ${this.balance}`);
-  }
-}
-
-// Creating an instance of the BankAccount class
-const myAccount = new BankAccount("20000", "Anthony Maina", 1000);
-
-// Accessing the properties in the Bank Account
-console.log(myAccount.accountNumber);  
-console.log(myAccount.accountHolder);  
-console.log(myAccount.balance);        
-
-// Calling methods in the Bank Account
-myAccount.deposit(500);                
-myAccount.withdraw(200);
-myAccount.getAccountNo();
 
 
-class SavingsAccount extends BankAccount{
-
-constructor(accountNumber, accountHolder, balance, interestRate){
-   super(accountNumber, accountHolder, balance)
-   this.interestRate = interestRate;
-
-}
-
-
-calculateInterest(){
-      const interest = (this.balance *this.interestRate)/ 100;
-      console.log(`The interest rate for your account is ${interest} `)
-
-}
+    });
+    return the_new_array
 
 
 }
 
-
-const theSpecifiedAccountSavings = new SavingsAccount("10000", "John Mwaniki", 7000, 5)
-
-theSpecifiedAccountSavings.deposit(500);                
-theSpecifiedAccountSavings.withdraw(200);
-theSpecifiedAccountSavings.getAccountNo();
-
-
-
-class checkingAccount extends BankAccount{
-
-  constructor(accountNumber, accountHolder, balance, overdraft){
-    //we use the superClass to ensure that we get the inherited properties
-     super(accountNumber, accountHolder, balance)
-     
-     this.overdraft = overdraft;
-  
+function look_for_Leap_year(year){
+  if((year % 4 === 0 && year % 100 !==0 ) || (year % 100 === 0 && year % 400 === 0)){
+    return year;
+  }else{
+    return false
   }
-  
-  
-  withdrawOverdraft(amount) {
-
-    const overdraftAdd = (this.balance - amount)>= -this.overdraft
-    if (this.balance >= amount || overdraftAdd) {
-      this.balance -= amount;
-      console.log (`Withdrawn ${amount} from account ${this.accountNumber}. New balance: ${this.balance}`);
-    } else {
-      console.log (`Insufficient funds in account ${this.accountNumber}. Current balance: ${this.balance}`);
-    }
-   
-    console.log(`Withdrawn amount${amount}`)
-  
-  }
-  
-  
-  }
-  
-  
-  const theCheckingAccounts = new checkingAccount("10000", "John Mwaniki", 7000, 200)
-  
+}
 
 
-  theCheckingAccounts.withdrawOverdraft(500)
-  theCheckingAccounts.deposit(5000)
-  theCheckingAccounts.getAccountNo()
+console.log(year_range_to_calculate_from(2001, 2023))
 
 
-  
-  
-  
-  
+
+//question four
+function calculate_sum_of_numbers(array_of_mixed_data_types){
+   let total = 0;
+
+
+   for (let i=0; i<= array_of_mixed_data_types.length; i++){
+       if (typeof array_of_mixed_data_types[i] === "number"){
+          total += array_of_mixed_data_types[i];
+
+       }
+   }
+   return total
+}
+
+array_of_mixed_data_types = [2, "11", 3, "a2", false, 5, 7, 1]
+console.log(`Original array: ${array_of_mixed_data_types}`)
+console.log(`Sum of all the numbers of the said array: ${calculate_sum_of_numbers(array_of_mixed_data_types)}`)
 
 
 
